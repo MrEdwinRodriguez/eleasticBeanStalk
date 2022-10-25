d2e17ce4a44ee5edf13281a1284ab6525bb70cb1
Edwin <mredwinrodriguez@gmail.com>
2022-10-25 16:14:40 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, (req, res) => {
+    console.log(`We are listenting at port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send(`This is the home page`);
+})
